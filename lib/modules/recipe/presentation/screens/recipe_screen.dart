338fe404b5fcec401f23e7f0e9c9ce090a24af9e
chimdikennacheta <cnnacheta@aellacredit.com>
2023-09-14 17:08:09 +0100

    Initial Commit

--- lib/modules/recipe/presentation/screens/recipe_screen.dart
+++ lib/modules/recipe/presentation/screens/recipe_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:recipe_app/modules/recipe/presentation/bloc/recipe_bloc.dart';
 import 'package:recipe_app/modules/recipe/presentation/widgets/recipe_card.dart';
@@ -15,29 +13,17 @@ class RecipeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-            'Recipe',
-            style: GoogleFonts.nunito(
-              textStyle: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    color: AppColors.buttonColorSecondary,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
+        title: Text(
+          'Recipe',
+          style: GoogleFonts.nunito(
+            textStyle: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  color: AppColors.buttonColorSecondary,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
           ),
-          leading: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: InkWell(
-              onTap: () {
-                context.read<RecipeBloc>().add(GetIngredientEvent());
-                Navigator.pop(context);
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: FaIcon(FontAwesomeIcons.arrowLeft),
-              ),
-            ),
-          )),
+        ),
+      ),
       body: ListView.builder(
           itemCount: recipe.recipe?.length,
           physics: const BouncingScrollPhysics(),
